d27b362ac49de5ca27e266697e3c85141d8d4b6c
Furkan Masraf <masraffurkan@gmail.com>
2026-07-06 02:38:00 +0300

    baseURL değiştirildi..

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://randevu-sistemi-dv33.onrender.com',
 });
 
 // Her istek gönderilmeden önce localStorage'daki güncel token'ı yakalayıp Header'a ekler
