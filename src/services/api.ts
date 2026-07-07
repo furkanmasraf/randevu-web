792080b9ffe8e5308778c6a981367d0419b3cb6f
Furkan Masraf <masraffurkan@gmail.com>
2026-07-07 17:10:39 +0300

    baseURL düzenlendi...

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-declare const process: { env: { NODE_ENV: string } } | undefined;
-
-const baseURL = 'https://randevu-sistemi-dv33.onrender.com';
-
+// Artık şüpheye yer yok, direkt canlı URL'i kullanıyoruz.
 const API = axios.create({
-  baseURL: baseURL,
-  withCredentials: true // Bu, hem local'de hem canlıda CORS sorununu çözmek için elzem.
+  baseURL: 'https://randevu-sistemi-dv33.onrender.com',
+  withCredentials: true 
 });
 
 API.interceptors.request.use((config) => {
-  if (config.url?.includes('/auth/login')) {
+  // Login ve Register isteklerinde token göndermiyoruz
+  if (config.url?.includes('/auth/login') || config.url?.includes('/auth/register')) {
     return config;
   }
 
