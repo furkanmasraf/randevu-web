d513df079d7a6e8738aafeebcf2542c69329f1ee
Furkan Masraf <masraffurkan@gmail.com>
2026-07-19 16:19:46 +0300

    feat: personel slot takvimi güncellendi...

--- src/pages/BarberDashboard.tsx
+++ src/pages/BarberDashboard.tsx
@@ -17,7 +17,7 @@ interface ServiceItem {
 export default function BarberDashboard() {
   const [busySlotsMap, setBusySlotsMap] = useState<Record<number, string[]>>({});
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
-  const timeSlots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
+  const timeSlots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00"," 22:30", "23:00", "23:30"];
   const [activeTab, setActiveTab] = useState<'appointments' | 'services' | 'hours' | 'employees' | 'settings'>('appointments');
   const [employees, _setEmployees] = useState<EmployeeItem[]>([]);
   const [services, _setServices] = useState<ServiceItem[]>([]);
